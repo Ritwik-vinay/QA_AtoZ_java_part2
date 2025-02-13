194670bcc801a4f8ed8e5fc80372f56fd96673f3
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-13 10:16:06 +0530

    Practice codes daily inputs

---
+++ src/Java001/class002_starPattern.java
@@ -0,0 +1,16 @@
+package Java001;
+
+public class class002_starPattern {
+    public static void main(String[] args) {
+        int rows=5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 1; j <=rows; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
