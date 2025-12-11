c542ceb309494511fa4019d745a3b0d19461461a
ritwik vinay <ritwikvinay38@gmail.com>
2025-12-11 11:48:58 +0530

    DailyPractice

---
+++ Sheet1/evenOdd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num= scn.nextInt();
+        evenOddClass eo= new evenOddClass();
+
+        eo.evenOrOdd(num);
+    }
+}
+class evenOddClass{
+    static void  evenOrOdd(int num ){
+        if (num%2==0){
+            System.out.println("Its a even number");
+        }
+        else {
+            System.out.println("it is an odd number");
+        }
+    }
+}
---
+++ Sheet1/primeNumber.java
@@ -0,0 +1,16 @@
+public class primeNumber {
+    public static void main(String[] args) {
+        primeNumberChecker pm = new primeNumberChecker();
+        System.out.println(pm.primeNumberChecker(5));
+    }
+}
+class primeNumberChecker{
+    boolean primeNumberChecker(int num){
+        for (int i=2; i<=num/2;i++){
+            if (num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
